07e81fbb9aefa2b28d904093949f48c4680e4ffb
Bob Rusch <git-commit-address>
2021-04-11 20:45:58 -0400

    working -- turn on  and off use Uppercase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  length: number = 0;
+  includeSymbols: boolean = false;
+  includeNumbers: boolean = false;
+  includeLetters: boolean = false;
+  includeUppers: boolean = false;
   password: string = '';
 
   onButtonClick() {
-    this.password = 'MY PASSWORD!!!';
+    const numbers: string = '1234567890';
+    const lowerLetters: string = 'abcdefghijklmnopqrstuvwyz';
+    const symbols: string = '!@#$%^&*()';
+
+    let validChars: string = '';
+    if (this.includeLetters) {
+      validChars += lowerLetters;
+    }
+    if (this.includeUppers) {
+      validChars += lowerLetters.toUpperCase();
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseUpper() {
+    this.includeUppers = !this.includeUppers;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
   }
 }
